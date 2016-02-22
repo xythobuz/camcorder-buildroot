4cd2397729eb78a6924fbfbc494b39cb72ff261a
Thomas Buck <xythobuz@xythobuz.de>
2016-02-22 21:58:08 +0100

    Add Dropbear, missing files.

---
+++ board/raspi/post_image.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp $BR2_EXTERNAL/board/raspi/boot/cmdline.txt  $BINARIES_DIR/rpi-firmware/cmdline.txt
+cp $BR2_EXTERNAL/board/raspi/boot/config.txt  $BINARIES_DIR/rpi-firmware/config.txt
+
